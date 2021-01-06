49685c73e9a03a7539339a6906f0987378d81785
chenming231 <chenming231@jd.com>
2021-01-06 13:53:24 +0800

    题目

--- src/main.go
+++ src/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"html/template"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func getSubNew(difficulty, max, no string) []string {
@@ -90,6 +93,29 @@ func getOne(difficulty, max int) string {
 	return r
 }
 
+func getRandomText(max int) []string {
+	data, err := ioutil.ReadFile("src/1.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return []string{}
+	}
+	var content = strings.Split(string(data), "\r\n")
+	s := make([]string, 0, max)
+	rand.Seed(time.Now().Unix())
+	var c = '①'
+	var count rune = 0
+	for i := 0; i < max; i++ {
+		s = append(s, string(c+count)+content[rand.Intn(len(content))]+"?")
+		count += 1
+	}
+	return s
+}
+
+type subject struct {
+	Item  []string
+	Logic []string
+}
+
 func main() {
 	r := gin.Default()
 	r.SetFuncMap(template.FuncMap{"mod": func(i, j int) bool { return i%j == 0 }, "add": func(i, j int) int { return i + j }})
@@ -104,6 +130,7 @@ func main() {
 				"difficulty": map[int]string{1: "单一加", 2: "单一减", 3: "单一加减混合", 4: "2连加", 5: "2连减", 6: "2连混合"},
 				"max":        []int{10, 20, 30, 50, 100},
 				"no":         []int{30, 50},
+				"logic":      []int{1, 2, 3, 5, 8},
 				"pages":      []int{1, 3, 5, 8, 10},
 			})
 		})
@@ -112,9 +139,10 @@ func main() {
 			max := c.Query("max")
 			no := c.Query("no")
 			page, _ := strconv.Atoi(c.Query("page"))
-			s := make([][]string, 0, page)
+			logic, _ := strconv.Atoi(c.Query("logic"))
+			s := make([]subject, 0, page)
 			for i := 0; i < page; i++ {
-				s = append(s, getSubNew(difficulty, max, no))
+				s = append(s, subject{Logic: getRandomText(logic), Item: getSubNew(difficulty, max, no)})
 			}
 			c.HTML(200, "my.html", gin.H{
 				"title": "练习",
